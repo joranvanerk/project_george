868e8cfd4e9d1329d8e47f7490f75c0aba619cf1
yusuf <321611@student.mboutrecht.nl>
2021-09-21 17:35:39 +0200

    colums toegevoegd bij about us page

--- aboutus.php
+++ aboutus.php
@@ -7,6 +7,38 @@
 <br>
 <img src="./img/banner.jpg" style="width:100%;"  alt="">
 
+<html>
+ <head>
+    <style>
+    {
+        box-sizing: border-box;
+    }
+    /* Set additional styling options for the columns*/
+    .column {
+    float: left;
+    width: 50%;
+    }
+
+    .row:after {
+    content: "";
+    display: table;
+    clear: both;
+    }
+    </style>
+ </head>
+ <body>
+    <div class="row">
+        <div class="column" >
+            <h2>Column 1</h2>
+            
+        </div>
+        <div class="column">
+            <h2>Column 2</h2>
+        
+        </div>
+    </div>
+ </body>
+</html>
 </body>
 
 <!-- include footer -->
