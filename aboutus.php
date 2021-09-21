8d5712f0424a1b0fbc672e9859e3d9199f4b4998
yusuf <321611@student.mboutrecht.nl>
2021-09-21 16:05:29 +0200

    img toegevoegd, about us page toegevoegd

---
+++ aboutus.php
@@ -0,0 +1,13 @@
+<!-- include framework css and bootstrap basic -->
+<?php include_once("./includes/framework.php"); ?>
+<!-- include header -->
+<?php include_once("./includes/header.php"); ?>
+<body>
+    
+<br>
+<img src="./img/banner.jpg" style="width:100%;"  alt="">
+
+</body>
+
+<!-- include footer -->
+<?php include_once("./includes/footer.php"); ?>
