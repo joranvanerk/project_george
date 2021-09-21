d1218855f950c81e3d875482d8ffa01abfae3f78
Joranvanerk <joranvanerk1@gmail.com>
2021-09-21 15:18:09 +0200

    Update header.php
    added header as it is at this moment

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,20 @@
 <div class="container">
-  <a style="border-top: 2px solid black;"></a>
+  <div style="background-color:#000000; height:1px; width:100%;"><br>
+    <nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <h1 class="george_title george_title_animation" href="./">George</h1>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<br><div style="background-color:#000000; height:1px; width:100%;">
 </div>
