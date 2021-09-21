f55154bd608fb4e642fe40d3fc77faf6790a6341
Turan <330506@student.mboutrecht.nl>
2021-09-21 21:05:39 +0200

    Added black line<br>Just need to fix the text alignment before merging"

--- contact.php
+++ contact.php
@@ -13,22 +13,20 @@
     <?php include_once("./includes/header.php"); ?>
     <!--The div element for the map -->
     <!--Google maps fix later-->
-    <div id="map"></div>
-
-    <h3>
-    <a style="float:left;text-align:left;">
-    Wilt uw contact nemen met ons?
+    <div id="black_line">
+    
+    <div  id="map"></div>
+      <h3>
+        Wilt uw contact nemen met ons?
         <br>Dat kunt uw op: contact@george-kanaleneiland.nl
         <br>Uw kunt met ons telefonisch contact nemen.
-        <br>Dat kan op: 030 – 28 15 100
-    </a>
-<a style="float:right;text-align:right;">
-    Waar kunt ons bezoeken?
-    <br>Dat kan op de Columbuslaan 540
-    <br>We zijn open maandag tot met vrijdag 18 tot 21 uur
-</a>
-    </h3>
-
+        <br>Dat kan op: 030 – 28 15 100<br>
+        <br>Waar kunt ons bezoeken?
+        <br>Dat kan op de Columbuslaan 540
+        <br>Maandag tot met vrijdag 18 tot 21 uur
+      </h3>
+      
+    </div>
     <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
     <script
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC5mH2r2cVrDH2p3n97i4tqFCa3wrACXSo&callback=initMap&libraries=&v=weekly"
@@ -46,4 +44,6 @@
     ></script>
 
   </body>
+  <!-- include footer -->
+<?php include_once("./includes/footer.php"); ?>
 </html>
