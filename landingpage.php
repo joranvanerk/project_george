f82006a18ef10facedd92441af05139c218bd08e
Steven Li <327068@student.mboutrecht.nl>
2021-09-22 12:41:51 +0200

    Created offset on landingspage from the navbar

--- landingpage.php
+++ landingpage.php
@@ -8,17 +8,17 @@
 <div class="container-fluid">
   <div class="container">
     <!-- First row of content -->
-    <div class="row">
+    <div class="row mb-2 mt-4">
       <div class="col-4">
-        <h2 class="george-title">Nieuwe sensatie</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tenetur unde reprehenderit quia, architecto modi quidem ipsam molestiae dolorem sit enim dolores vitae eum culpa? Perferendis, tempore reprehenderit. Dolorum, asperiores.</p>
+        <h2 class="george_title">Nieuwe sensatie</h2>
+        <p class="george">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tenetur unde reprehenderit quia, architecto modi quidem ipsam molestiae dolorem sit enim dolores vitae eum culpa? Perferendis, tempore reprehenderit. Dolorum, asperiores.</p>
       </div>
       <div class="col-4">
         <img src="./image/landingPageSaleV1.png" class="img-fluid rounded mx-auto d-block" alt="...">
       </div>
       <div class="col-4">
-        <h2 class="george-title">Reserveer nu!</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo maiores reprehenderit earum tenetur aliquid ipsam, vero ipsa quam voluptate, aspernatur inventore officia ex modi autem voluptatem illo, laudantium ea mollitia?</p>
+        <h2 class="george_title">Reserveer nu!</h2>
+        <p class="george">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo maiores reprehenderit earum tenetur aliquid ipsam, vero ipsa quam voluptate, aspernatur inventore officia ex modi autem voluptatem illo, laudantium ea mollitia?</p>
       </div>
     </div>
     <!-- Second row of content -->
@@ -30,8 +30,8 @@
     <!-- Third row of content -->
     <div class="row">
       <div class="col-6">
-        <h2 class="george-title">69 December gaan wij open!</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ut ipsam nesciunt dolorum fugiat nihil repudiandae esse, omnis quisquam quidem nemo fugit placeat? Animi repellendus, ad nostrum porro exercitationem cum!</p>
+        <h2 class="george_title">69 December gaan wij open!</h2>
+        <p class="george">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ut ipsam nesciunt dolorum fugiat nihil repudiandae esse, omnis quisquam quidem nemo fugit placeat? Animi repellendus, ad nostrum porro exercitationem cum!</p>
         </div>
       <div class="col-6">
         <img src="." class="landingPageSale img-fluid" alt="Landing Page Sale"> 
