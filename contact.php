4f41f094e05a1fe76e0103d89f101e30e08b4e11
Turan <330506@student.mboutrecht.nl>
2021-09-22 12:25:34 +0200

    Fixed the blackline issue

--- contact.php
+++ contact.php
@@ -12,19 +12,33 @@
     <!-- include header -->
     <?php include_once("./includes/header.php"); ?>
     <!--The div element for the map -->
+    <div class="container">
+      <br>
+      <div class="row">
+        <div class="col-5">
+          <h3>
+            Wilt uw contact nemen met ons?
+            <br>Dat kunt uw op: contact@george-kanaleneiland.nl
+            <br>Uw kunt met ons telefonisch contact nemen.
+            <br>Dat kan op: 030 – 28 15 100<br>
+            <br>Waar kunt ons bezoeken?
+            <br>Dat kan op de Columbuslaan 540
+            <br>Maandag tot met vrijdag 18 tot 21 uur
+          </h3>
+        </div>
+        <div class= "col-2">
+          <div id="black_line"></div>
+        </div>
+        <div class="col-5">
+          <div  id="map"></div>
+        </div>
+      </div>
+    </div>
+</div>
     <!--Google maps fix later-->
-    <div id="black_line">
     
-    <div  id="map"></div>
-      <h3>
-        Wilt uw contact nemen met ons?
-        <br>Dat kunt uw op: contact@george-kanaleneiland.nl
-        <br>Uw kunt met ons telefonisch contact nemen.
-        <br>Dat kan op: 030 – 28 15 100<br>
-        <br>Waar kunt ons bezoeken?
-        <br>Dat kan op de Columbuslaan 540
-        <br>Maandag tot met vrijdag 18 tot 21 uur
-      </h3>
+    
+
       
     </div>
     <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
