900c5c806a69074ca20a1cf9b64a9f353a1009f6
Joranvanerk <joranvanerk1@gmail.com>
2021-09-22 10:20:17 +0200

    Update header.php
    updated header with all comments, file is done.

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,8 @@
+<!-- container with content added -->
 <div class="container">
+  <!-- simple horizontal line -->
   <div style="background-color:#000000; height:1px; width:100%;"><br>
+    <!-- start of the navbar in the header -->
     <nav class="navbar navbar-expand-lg navbar-light">
   <div class="container-fluid">
     <h1 class="george_title george_title_animation" href="./">George</h1>
@@ -9,19 +12,31 @@
     <div class="collapse navbar-collapse justify-content-end" id="nav">
       <div class="navbar-nav">
         <a class="nav-link george_menu george_menu_active" aria-current="page" href="#">Home</a>
+        <!-- simple vertical line -->
         <div class="vl"></div>
+        <!-- navigation element for redirection to page -->
         <a class="nav-link george_menu" href="aboutus">About us</a>
+        <!-- simple vertical line -->
         <div class="vl"></div>
+        <!-- navigation element for redirection to page -->
         <a class="nav-link george_menu" href="contact">Contact</a>
+        <!-- simple vertical line -->
         <div class="vl"></div>
+        <!-- navigation element for redirection to page -->
         <a class="nav-link george_menu" href="menu">Menu</a>
+        <!-- simple vertical line -->
         <div class="vl"></div>
+        <!-- navigation element for redirection to page -->
         <a class="nav-link george_menu" href="reservation">Reservation</a>
+        <!-- simple vertical line -->
         <div class="vl"></div>
+        <!-- navigation element for redirection to page -->
         <a class="nav-link george_menu" href="bookevent">Book an event</a>
       </div>
     </div>
   </div>
+  <!-- end of the navbar in the header -->
 </nav>
+<!-- simple horizontal line -->
 <br><div style="background-color:#000000; height:1px; width:100%;">
 </div>
