f5ef1ba4644af2690b08ed3458e7ae9b3d130c44
yusuf <321611@student.mboutrecht.nl>
2021-09-22 14:09:19 +0200

    nieuwe imgs toegevoegd en collumns bewerkt bij about us page fotos toegevoegd aan de pagina

--- aboutus.php
+++ aboutus.php
@@ -3,46 +3,54 @@
 <!-- include header -->
 <?php include_once("./includes/header.php"); ?>
 
-<!-- begin banner-->
-<div class="container">
-
-
-<br>
-<img src="./img/banner.jpg" style="width:100%;"  alt="">
-<!--einde banner-->
-</div>
-     <!--begin columns-->
-     <div class="container">
-    <div class="row">
-        <div class=" col-sm-12 col-md-6" >
+        <!-- begin banner-->
+        <div class="container">
+        <br>
+        <img src="./img/banner.jpg" style="width:100%;"  alt=""> 
+        <!--einde banner-->
+        </div>
+        <!--begin columns-->
+        <br> <br> 
+        <div class="container">
+        <div class="row">
+        <div class="col-sm-12 col-md-6" >
             <br>
             <!-- dit is de titel van de tekst -->
             <h2>The beginning</h2>
-            <!--tekst die je kan aanpassen tussen h2 tags-->
+            <!--tekst die je kan aanpassen tussen p tags-->
             <p> 
                 about us about us about us about us about us 
                 about us about us about us about us about us 
                 about us about us about us about us about us 
                 about us about us about us about us about us 
+                about us about us about us about us about us
+                about us about us about us about us about us
+                about us about us about us about us about us
+                about us about us about us about us about us
                 about us about us about us about us about us
                 about us about us about us about us about us
                 about us about us about us about us about us
             </P>
         </div>
-</div>
-    <div class="row">
-        <div class="col-6 col-sm-12 col-md-6">
-
+        <img src="./img/begin.jpg" class=" " style="width:40%;"  alt=""> 
         </div>
-        <div class=" col-6 col-sm-12 col-md-6" >
-            <br>
+        <div class="row">
+        <div class="col-sm-12 col-md-6">
+        <img src="./img/mission .jpg" style="width:100%; height: auto;"  alt=""> 
+        </div>
+        <div class="col-sm-12 col-md-6" >
+        
+        
             <!-- dit is de titel van de tekst -->
             <h2>Mission and Succes</h2>
-            <!--tekst die je kunt aanpassen tussen h2 tags-->
+            <!--tekst die je kunt aanpassen tussen p tags-->
             <p> 
                 about us about us about us about us about us 
                 about us about us about us about us about us 
                 about us about us about us about us about us 
+                about us about us about us about us about us
+                about us about us about us about us about us
+                about us about us about us about us about us
                 about us about us about us about us about us
                 about us about us about us about us about us
                 about us about us about us about us about us
@@ -51,9 +59,9 @@
             
         <!--einde columns-->
         </div>
-    </div>
-    </div>
-    </div>
+        </div>
+        </div>
+        </div>
 
 <!-- include footer -->
 <?php include_once("./includes/footer.php"); ?>
