5f40f8b756c95f468f6b3d2dfd3b172277763400
325972 <325972@student.mboutrecht.nl>
2021-09-23 13:56:15 +0200

    Col SM and MD added to menu.php

--- menu.php
+++ menu.php
@@ -7,56 +7,56 @@
 <div class="container">
 <h1 id="title_menu"> George Menu </h1>
 <div class="row">
-<div class="col-3">
+<div class="col-3 col-sm-12 col-md-6">
   <img src="./img/Burger1.png" style="max-width: 200px;" class="" alt="...">
   <div class="card-body">
     <h5 class="card-title">IRISH BEEF</h5>
     <p class="card-text">The pure, full flavor of 1OO% prime and tender Irish Beef.</p>
 </div>
 </div>
-<div class="col-3">
+<div class="col-3 col-sm-12 col-md-6">
   <img src="./img/Burger2.png" style="max-width: 200px;" class="" alt="...">
   <div class="card-body">
     <h5 class="card-title">ANGUS BURGER</h5>
     <p class="card-text">The succulent free range taste and tenderness of premium aged Black Angus US beef.</p>
 </div>
 </div>
-<div class="col-3">
+<div class="col-3 col-sm-12 col-md-6">
   <img src="./img/Burger3.png" style="max-width: 200px;" class="" alt="...">
   <div class="card-body">
     <h5 class="card-title">WAGYU BURGER</h5>
     <p class="card-text">Widely believed, by many, to be the highest quality beef in the world – a true-melt-in-the-mouth sensation.</p>
 </div>
 </div>
-<div class="col-3">
+<div class="col-3 col-sm-12 col-md-6">
   <img src="./img/Burger3.png" style="max-width: 200px;" class="" alt="...">
   <div class="card-body">
     <h5 class="card-title">CHICKEN SANDWICH</h5>
     <p class="card-text">Served with homemade lemon-honey-garlic sauce.</p>
 </div>
 </div>
-<div class="col-3">
+<div class="col-3 col-sm-12 col-md-6">
   <img src="./img/Burger4.png" style="max-width: 200px;" class="" alt="...">
   <div class="card-body">
     <h5 class="card-title">BEYOND BURGER</h5>
     <p class="card-text">The famous Beyond burger. 100% vegan, 100% taste</p>
 </div>
 </div>
-<div class="col-3">
+<div class="col-3 col-sm-12 col-md-6">
   <img src="./img/Burger5.png" style="max-width: 200px;" class="" alt="...">
   <div class="card-body">
     <h5 class="card-title">PORTOBELLO</h5>
     <p class="card-text">For the vegetarians among us, but also when you want to try something new. Be surprised of it’s great taste!</p>
 </div>
 </div>
-<div class="col-3">
+<div class="col-3 col-sm-12 col-md-6">
   <img src="./img/Burger8.png" style="max-width: 200px;" class="" alt="...">
   <div class="card-body">
     <h5 class="card-title">SIDE DISH</h5>
     <p class="card-text">Combine your burger with a great variety of side dishes, like homemade fries, onion rings or a fresh salad.</p>
 </div>
 </div>
-<div class="col-3">
+<div class="col-3 col-sm-12 col-md-6">
   <img src="./img/Burger7.png" style="max-width: 200px;"  class="" alt="...">
   <div class="card-body">
     <h5 class="card-title">VEGABURGER</h5>
