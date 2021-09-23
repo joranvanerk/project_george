b5c79242ad2718ceddd3184230d446b3ff6365e1
Joranvanerk <joranvanerk1@gmail.com>
2021-09-23 09:57:32 +0200

    Update contact.php
    Contact page updated the text

--- contact.php
+++ contact.php
@@ -18,12 +18,12 @@
         <div class="col-5">
           <h3>
             Wilt uw contact nemen met ons?
-            <br>Dat kunt uw op: <br>contact@george-kanaleneiland.nl<br>
-            <br>Uw kunt met ons telefonisch contact nemen.
-            <br>Dat kan op: 030 – 28 15 100<br>
+            <br>wij zijn bereikbaar via de mail <br><a href="mailto: contact@george-kanaleneiland.nl">contact@george-kanaleneiland.nl</a><br>
+            <br>Uw kunt ook telefonisch met ons contact opnemen,
+            <br>Via: 030 – 28 15 100<br>
             <br>Waar zijn wij te vinden?
-            <br>Dat kan op de Columbuslaan 540
-            <br>Maandag tot met vrijdag 18 tot 21 uur
+            <br>Wij zijn gevestigd op Columbuslaan 540
+            <br>Maandag tot met vrijdag 18:00 tot 21:00 uur
           </h3>
         </div>
         <div class= "col-2">
@@ -36,17 +36,17 @@
     </div>
 </div>
     <!--Google maps fix later-->
-    
-    
 
-      
+
+
+
     </div>
     <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
     <script
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC5mH2r2cVrDH2p3n97i4tqFCa3wrACXSo&callback=initMap&libraries=&v=weekly"
       async
     ></script>
-    
+
     <script
       function initMap() {
           var location = {lat: 52.063780, lng:5.100000};
