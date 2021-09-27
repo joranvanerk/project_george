19d9a4a4ef09048955bf3037feffb45ad49e0859
Joranvanerk <joranvanerk1@gmail.com>
2021-09-27 16:05:55 +0200

    Update header.php
    added link to basic logo of george

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <!-- start of the navbar in the header -->
     <nav class="navbar navbar-expand-lg navbar-light">
   <div class="container-fluid">
-    <h1 class="george_title george_title_animation" href="./">George</h1>
+    <a  href="./" style="text-decoration: none;"><h1 class="george_title george_title_animation">George</h1></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
