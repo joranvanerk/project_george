cfb7ebc10d30bf53fbf504f398f4c22afc423626
Steven Li <327068@student.mboutrecht.nl>
2021-09-27 15:29:57 +0200

    landingspage has been made responsive

--- landingpage.php
+++ landingpage.php
@@ -3,18 +3,17 @@
   <div class="container">
     <!-- First row of content -->
     <div class="row mb-2 mt-4">
-      <div class="col-4">
+      <div class="col-sm-12 col-md-4 cold-lg-4">
         <h2 class="george_title">Nieuwe sensatie</h2>
         <p class="george">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tenetur unde reprehenderit quia, architecto modi quidem ipsam molestiae dolorem sit enim dolores vitae eum culpa? Perferendis, tempore reprehenderit. Dolorum, asperiores.</p>
       </div>
-      <div class="col-4 vl">
+      <div class="col-sm-12 col-md-4 cold-lg-4 vl">
         <img src="./image/landingPageSaleV1.png" class="img-fluid rounded mx-auto d-block" alt="...">
       </div>
-      <div class="col-4 vl">
+      <div class="col-sm-12 col-md-4 cold-lg-4 vl">
         <h2 class="george_title">Reserveer nu!</h2>
         <br>
         <a class="btn btn-outline-george btn-lg" href="reservation" role="button">Reserveer zitplaatsen</a>
-        <p class="george"></p>
       </div>
     </div>
     <!-- Second row of content -->
@@ -26,12 +25,12 @@
     </div>
     <!-- Third row of content -->
     <div class="row">
-      <div class="col-6">
+      <div class="col-sm-12 col-md-6 col-lg-6">
         <div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
         <h2 class="george_title">69 December gaan wij open!</h2>
         <p class="george">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ut ipsam nesciunt dolorum fugiat nihil repudiandae esse, omnis quisquam quidem nemo fugit placeat? Animi repellendus, ad nostrum porro exercitationem cum!</p>
         </div>
-      <div class="col-6 vl">
+      <div class="col-sm-12 col-md-6 col-lg-6 vl">
         <div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
         <img src="." class="landingPageSale img-fluid" alt="Landing Page Sale">
         </div>
