c22e6b5a257500fee37fd7a2896ac0e94693ac75
yusuf <321611@student.mboutrecht.nl>
2021-09-27 16:20:03 +0200

    reserveren pagina aangepast

--- reservation.php
+++ reservation.php
@@ -7,48 +7,34 @@
         <div class="container">
             <br>
         <img src="./img/reserveren.jpg" class= "img-fluid"  alt=""> 
-        <!--einde banner-->
-        
         </div>
+         <!--einde banner-->
+
+
         <!--begin columns-->
         <br><br>
-        <div class="container">
-        <div class="row">
-        <div class="col-sm-12 col-md-6" >
-        <div style="background-color:#000000; height:1px; width:100%;"></div>
 <!-- form begin naam achternaam  -->
-    <h3 style="text-align: center">Reserveren</h3>
-    <br>
-    <div class="row">
-        <div class="col">
-            <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+<div class="container">
+    <div class=" row"> <div style="background-color:#000000; height:1px; width:100%;"></div>
+        <!-- Formulier voor reserveren -->
+        <div class="col-sm-12 col-md-6 ">
+            <h3 style="text-align: center">Reserveren</h3>
+            <br>
+            <input type="text" class="form-control mb-2" placeholder="voornaam" aria-label="First name">
+            <input type="text" class="form-control mb-2" placeholder="achternaam" aria-label="Last name">
+            <input type="text" class="form-control mb-2" placeholder="vul hier uw email in" aria-label="e-mail">
+            <input type="text" class="form-control mb-2" placeholder="+31 mobiel" aria-label="tel">
         </div>
-        <div class="col">
-            <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
-        </div>
+        <!-- Foto/tekst -->
+        <div class=" vl col-sm-12 col-md-6">
+        <h3 style="text-align:center">je kanker moeder</h3>
         </div>
     </div>
+</div>
 <!-- form einde naam achternaam -->
 
 
 
-        <div class="vll col-sm-12 col-md-6">
-        <div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
-            <img src=""  alt="">
-        </div>
-         
-        </div>
-        <div class="row">
-        <div class="col-sm-12 col-md-6">
-        <img src="" class="img-fluid"  alt=""> 
-        </div>
-        <div class="vll col-sm-12 col-md-6" >
-        
-        </div>
-        </div>
-        </div>
-        </div>
-        <!--einde columns-->
-
+       
 <!-- include footer -->
 <?php include_once("./includes/footer.php"); ?>
