4d99775712c678b2d204bb1efff04e211b90daf6
325972 <325972@student.mboutrecht.nl>
2021-09-27 15:48:36 +0200

    Animations and allergies added menu.php

--- menu.php
+++ menu.php
@@ -8,62 +8,65 @@
 <h1 id="title_menu"> George Menu </h1>
 <div class="row">
 <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
-  <img src="./img/Burger1.png" style="max-width: 200px;" class="" alt="...">
+  <img src="./img/Burger1.png" style="max-width: 200px;" class="animenu" alt="...">
   <div class="card-body">
-    <h5 class="card-title">IRISH BEEF</h5>
+    <h5 class="card-title">IRISH BEEF €8,35</h5>
     <p class="card-text">The pure, full flavor of 1OO% prime and tender Irish Beef.</p>
 </div>
 </div>
 <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
-  <img src="./img/Burger2.png" style="max-width: 200px;" class="" alt="...">
+  <img src="./img/Burger2.png" style="max-width: 200px;" class="animenu" alt="...">
   <div class="card-body">
-    <h5 class="card-title">ANGUS BURGER</h5>
+    <h5 class="card-title">ANGUS BURGER €10,45</h5>
     <p class="card-text">The succulent free range taste and tenderness of premium aged Black Angus US beef.</p>
 </div>
 </div>
 <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
-  <img src="./img/Burger3.png" style="max-width: 200px;" class="" alt="...">
+  <img src="./img/Burger3.png" style="max-width: 200px;" class="animenu" alt="...">
   <div class="card-body">
-    <h5 class="card-title">WAGYU BURGER</h5>
+    <h5 class="card-title">WAGYU BURGER €17,95</h5>
     <p class="card-text">Widely believed, by many, to be the highest quality beef in the world – a true-melt-in-the-mouth sensation.</p>
 </div>
 </div>
 <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
-  <img src="./img/Burger3.png" style="max-width: 200px;" class="" alt="...">
+  <img src="./img/Burger3.png" style="max-width: 200px;" class="animenu" alt="...">
   <div class="card-body">
-    <h5 class="card-title">CHICKEN SANDWICH</h5>
+    <h5 class="card-title">CHICKEN SANDWICH €6,75</h5>
     <p class="card-text">Served with homemade lemon-honey-garlic sauce.</p>
 </div>
 </div>
 <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
-  <img src="./img/Burger4.png" style="max-width: 200px;" class="" alt="...">
+  <img src="./img/Burger4.png" style="max-width: 200px;" class="animenu" alt="...">
   <div class="card-body">
-    <h5 class="card-title">BEYOND BURGER</h5>
+    <h5 class="card-title">BEYOND BURGER €15,75</h5>
     <p class="card-text">The famous Beyond burger. 100% vegan, 100% taste</p>
 </div>
 </div>
 <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-lg-">
-  <img src="./img/Burger5.png" style="max-width: 200px;" class="" alt="...">
+  <img src="./img/Burger5.png" style="max-width: 200px;" class="animenu" alt="...">
   <div class="card-body">
-    <h5 class="card-title">PORTOBELLO</h5>
+    <h5 class="card-title">PORTOBELLO €12,95</h5>
     <p class="card-text">For the vegetarians among us, but also when you want to try something new. Be surprised of it’s great taste!</p>
 </div>
 </div>
 <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
-  <img src="./img/Burger8.png" style="max-width: 200px;" class="" alt="...">
+  <img src="./img/Burger8.png" style="max-width: 200px;" class="animenu" alt="...">
   <div class="card-body">
-    <h5 class="card-title">SIDE DISH</h5>
-    <p class="card-text">Combine your burger with a great variety of side dishes, like homemade fries, onion rings or a fresh salad.</p>
+    <h5 class="card-title">SIDE DISH €6.95</h5>
+    <p class="card-text">Combine your burger with a great variety of side dishes, like homemade fries, onion rings or a fresh salad. </p>
 </div>
 </div>
 <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
-  <img src="./img/Burger7.png" style="max-width: 200px;"  class="" alt="...">
+  <img src="./img/Burger7.png" style="max-width: 200px;"  class="animenu" alt="...">
   <div class="card-body">
-    <h5 class="card-title">VEGABURGER</h5>
+    <h5 class="card-title">VEGABURGER €6,75</h5>
     <p class="card-text">Another delicious vegetarian burger, made of chickpeas.</p>
 </div>
 </div>
   </div>
+  <img src="./img/iconallergies1.jpg" style="max-width: 200px;"  class="allergies" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Allergies</h5>
 </div>
 </body>
 
