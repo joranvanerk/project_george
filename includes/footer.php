158f0f35d27ffcedda069999b9f8704c57a3788b
Steven Li <327068@student.mboutrecht.nl>
2021-09-28 14:48:47 +0200

    Added footer format with a sitemap

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,47 @@
-
+<!-- Footer -->
+<div class="container">
+  <div class="row">
+  <div class="mt-2 mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
+    <!-- Newsletter -->
+    <div class="col-4">
+      <h5 class="mt-4 george_title">Join our newsletterrrrrr</h5>
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+    <div class="col-4">
+      <ul class="list-group">
+        <li class="list-group-item"><a href="aboutus">About us</a></li>
+        <li class="list-group-item"><a href="menu">Our Menu</a></li>
+        <li class="list-group-item"><a href="reservation">Reserve a spot</a></li>
+        <li class="list-group-item"><a href="bookevent">Book a venue</a></li>
+      </ul>
+    </div>
+    <div class="col-4">
+      <ul class="list-group">
+        <li class="list-group-item"><a href="general-terms">General Terms</a></li>
+        <li class="list-group-item"><a href="privacy-policy">Privacy Policy</a></li>
+        <li class="list-group-item"><a href="cookies">Cookies Policy</a></li>
+        <li class="list-group-item"><a href="unsubscribe">Unsubscribe from newsletter</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<!-- Footer Copyright -->
+<div class="container">
+  <div class="row">
+    <div class="mt-2 mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
+    <h4 class="george_title mt-2">Copyright 2021</h4>
+</div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 <script src="./js/app.js"></script>
