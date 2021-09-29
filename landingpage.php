4d27a130027f565166ea0d9dc3fb94fee1887636
Steven Li <327068@student.mboutrecht.nl>
2021-09-29 10:55:40 +0200

    changed the text on landingpage

--- landingpage.php
+++ landingpage.php
@@ -5,7 +5,7 @@
     <div class="row mb-2 mt-4">
       <div class="col-sm-12 col-md-4 cold-lg-4">
         <h2 class="george_title">Nieuwe sensatie</h2>
-        <p class="george">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tenetur unde reprehenderit quia, architecto modi quidem ipsam molestiae dolorem sit enim dolores vitae eum culpa? Perferendis, tempore reprehenderit. Dolorum, asperiores.</p>
+        <p class="george_menu no-margin mt-4">Op onze openingsdag gaan wij spectaculaire gerechten aanbieden van de hoogste kwaliteit, maar voor de halve prijs!</p>
       </div>
       <div class="col-sm-12 col-md-4 cold-lg-4 vl">
         <img src="./img/landingPageSale.png" class="img-fluid rounded mx-auto d-block" alt="...">
@@ -27,13 +27,15 @@
     <div class="row">
       <div class="col-sm-12 col-md-6 col-lg-6 mt-2">
         <div class="mb-3" style="background-color:#000000; height:1px; width:100%;"></div>
-        <h2 class="george_title mt-2">69 December gaan wij open!</h2>
-        <p class="george">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ut ipsam nesciunt dolorum fugiat nihil repudiandae esse, omnis quisquam quidem nemo fugit placeat? Animi repellendus, ad nostrum porro exercitationem cum!</p>
+        <h2 class="george_title mt-2">12 December gaan wij open!</h2>
+        <p class="george_menu no-margin mt-4">
+          Mis je kans niet om op onze openingsdag voor de halve prijs te kunnen dineren! Reserveer nu!
+        </p>
         </div>
       <div class="col-sm-12 col-md-6 col-lg-6 vl mt-2">
         <div class="mb-3" style="background-color:#000000; height:1px; width:100%;"></div>
         <img src="./img/flower.png" class="landingPageSale img-fluid" alt="Landing Page Sale">
-        </div>
+      </div>
     </div>
 
   </div>
