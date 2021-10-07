2864041e1f51eb92e72263aaa60f1c33183bd2bf
Steven Li <327068@student.mboutrecht.nl>
2021-10-07 12:28:08 +0200

    Finished first draft of registered.php

--- registered.php
+++ registered.php
@@ -9,31 +9,41 @@ include_once("./includes/header.php");
 ?>
 
 <div class="container">
-  <div class="row">
+  <div class="row mb-2 mt-4">
+    <!-- Thank you text after fully registering your account -->
     <div class="col-12">
-      <p class="george-menu">
-      Thank you for finishing your registation process at George Kanaleneiland! We'd like to officially welcome you to the Georgies!
+    <h5 class="george_title mb-3">Thank you!</h5>
+    <h5 class="george_menu" style="font-weight: 200; text-transform: none; margin-left: 0; margin-top: 0.2rem;">
+      We'd like to officially welcome you to the Georgies! Your account information has been successfully submitted.
       <br><br>
-      If your selected desire role is Student, Companion or Teacher, this will be verified by our staff first. Once verified you will be send an e-mail.
-      </p>
+      If your desired role is stated down below, it must be verified by our staff first.
+      <ul class="mt-2">
+        <li>Student</li>
+        <li>Companion</li>
+        <li>Teacher</li>
+      </ul>
+    </h5>
+    <h5 class="george_menu" style="font-weight: 600; margin-left: 0; color: red !important;">Have a great day! 🎈</h5>
     </div>
   </div>
-  <div class="row">
-    <div class="col-6">
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item"><a class="george_link" href="">Link to x</a></li>
-      <li class="list-group-item"><a class="george_link" href="">Link to x</a></li>
-      <li class="list-group-item"><a class="george_link" href="">Link to x</a></li>
-      <li class="list-group-item"><a class="george_link" href="">Link to x</a></li>
-      <li class="list-group-item"><a class="george_link" href="">Link to x</a></li>
-    </ul>
+  <!-- Horizontal Line -->
+  <div style="background-color:#000000; height:1px; width:100%;"></div>
+  <div class="row mt-2 mb-2">
+    <div class="col-sm-12 col-md-5">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"><i class="fas fa-user"></i> <a class="george_link" href="./mygeorge">My George</a></li>
+        <li class="list-group-item"><i class="far fa-calendar-check"></i> <a class="george_link" href="./bookevent">Book an event</a></li>
+        <li class="list-group-item"><i class="fas fa-table"></i> <a class="george_link" href="./reservation">Reserve a table</a></li>
+        <li class="list-group-item"><i class="fas fa-phone"></i> <a class="george_link" href="./contact">Contact us</a></li>
+        <li class="list-group-item"><i class="fas fa-book-open"></i> <a class="george_link" href="./menu">Our Menu</a></li>
+      </ul>
     </div>
-    <div class="col-6">
-    Did you know: 
-    In My George you can check your reservations, current actions, dinner points and many more!
-    <br><br>
-    xoxo
-    George Kanaleneiland
+    <div class="col-sm-12 col-md-5 vl">
+      <h5 class="george_title mt-2 mb-3" style="font-weight: 400; text-transform: none; margin-left: 0; margin-top: 0.2rem;">Did you know:</h5>
+      <h5 class="george_menu mb-4" style="font-weight: 400; text-transform: none; margin-left: 0; margin-top: 0.2rem;">In My George you can check your reservations, current actions, dinner points and many more!</h5>
+      
+      <h5 class="george_menu" style="font-weight: 400; text-transform: none; margin-left: 0; margin-top: 0.2rem;">We would love to hear more from your event and organisation, and always remember, need any help? Just contact us!</h5>
+      <h5 class="george_menu" style="font-weight: 600; margin-left: 0; color: red !important;">GOOD LUCK ❤️</h5>
     </div>
   </div>
 </div>
