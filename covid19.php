238af6159949527110a3c6876238cb622cd3282d
Joranvanerk <joranvanerk1@gmail.com>
2021-10-25 16:31:35 +0200

    Update covid19.php
    added covid19 page content

--- covid19.php
+++ covid19.php
@@ -8,7 +8,29 @@ $active_page_filename = basename(__FILE__);
 <!-- include header -->
 <?php include_once("./includes/header.php"); ?>
 
+<div class="container">
+  <br>
+  <div class="row">
+    <div class="col-sm-12 col-md-7">
+      <br>
+      <h3 class="george_title bookeventani" style="margin-top: 4rem; margin-bottom: 4rem;">Information about the <u>Covid-19 virus</u></h3>
+      <br>
+      <h3 class="george_menu bookeventani" style="font-weight: 400; text-transform: none; margin-left: 0;">To get to know about our current rules in <u>all our restaurants</u>, want more specific information before you <u>visit</u> our Amsterdam styled restaurants? Contact us through our <u>Contact page</u>.</h3>
+      <br>
+      <h3 class="george_menu bookeventani" style="font-weight: 600; margin-left: 0; margin-top: 5rem;">Greetings from <u>Amsterdam</u></h3>
+      <h3 class="george_menu bookeventani" style="font-weight: 600; margin-left: 0; color: red !important;">XOXO 💋</h3>
+    </div>
+    <div class="col-1" style="margin-right: -5rem;">
+      <div class="vl mobielhide" style="height: 95%;"></div>
+    </div>
+    <div class="col-md-4 col-sm-12">
+      <br>
+      <h3 class="george_title bookeventani" style="margin-top: 1.5rem; margin-bottom: 1.5rem;">Let us know</h3>
 
+    </div>
+  </div>
+</div>
+<br><br>
 
 <!-- include footer -->
 <?php include_once("./includes/footer.php"); ?>
