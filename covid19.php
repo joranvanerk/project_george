80221be8eebfe9aeb17546ea4a4abcf75329d872
Joranvanerk <joranvanerk1@gmail.com>
2021-10-25 16:06:22 +0200

    Update covid19.php
    added header element

---
+++ covid19.php
@@ -0,0 +1,9 @@
+<!-- include framework css and bootstrap basic -->
+<?php include_once("./includes/framework.php");
+
+// basefile management system for header
+$active_page_filename = basename(__FILE__);
+
+ ?>
+<!-- include header -->
+<?php include_once("./includes/header.php"); ?>
