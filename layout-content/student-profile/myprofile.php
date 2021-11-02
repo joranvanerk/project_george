0de481ec0e32c2f568d38c03ff772f5466510166
Steven Li <327068@student.mboutrecht.nl>
2021-11-02 16:48:07 +0100

    Added student-profile map with seperate pages for different subjects

---
+++ layout-content/student-profile/myprofile.php
@@ -0,0 +1,54 @@
+<div class="container student-info">
+  <div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
+  <div class="row">
+    <div class="col-12">
+      <h2>My Profile</h2>
+      <div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
+    </div>
+  </div>
+  <div class="row row-info">
+    <div class="col-sm-6 offset-sm-0 col-md-2 offset-md-1">
+      <ul class="student-header">
+          <li>Student no.:</li>
+          <li>Name:</li>
+          <li>E-mail:</li>
+          <li>Phone number:</li>
+          <li>Address:</li>
+          <li>Role:</li>
+          <li>Teacher:</li>
+          <li>Lesson Package:</li>
+        </ul>
+      </div>
+      <div class="col-sm-6 col-md-3">
+        <ul class="student-info">
+        <li>327068</li>
+        <li>Steven Li</li>
+        <li>327068@student.mboutrecht.nl</li>
+        <li>06123568</li>
+        <li>Teststraat 73</li>
+        <li>Student</li>
+        <li>HSOK</li>
+        <li>Chef</li>
+      </ul>
+      </div>
+    <div class="col-6 vl mt-3 mb-3 mobielhide">
+      <img src="./img/student-picture/default.png" class="img-fluid" alt="Student Image">
+    </div>
+  </div>
+</div>
+
+<div class="container student-options">
+  <div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
+  <div class="row">
+    <div class="col-6 mt-3 mb-3">
+      <h1 class="student-text text-upper">Book a work night</h1>
+      <a class="btn btn-outline-george btn-lg" href="#bookshift">Book a shift</a>
+      <a class="btn btn-outline-george btn-lg" href="#signoff">Sign out of shift</a>
+    </div>
+    <div class="col-6 vl mt-3 mb-3">
+      <h1 class="student-text text-upper">Quick Links</h1>
+      <a class="btn btn-outline-george btn-lg" href="https://www.mboutrecht.nl">MBO Utrecht</a>
+      <a class="btn btn-outline-george btn-lg" href="https://www.georgemarina.nl">George Marina</a>
+    </div>
+  </div>
+</div>
--- student-profile.php
+++ student-profile.php
@@ -10,67 +10,41 @@ include("./includes/header.php"); ?>
 
 <div class="container">
   <ul class="student-nav">
-    <li><a href="student-profile">My Profile</a></li>
-    <li><a href="student-profile#results">Results</a></li>
-    <li><a href="student-profile#progress">Progress</a></li>
-    <li><a href="student-profile#mail">Mail</a></li>
-    <li><a href="student-profile#edit">Edit</a></li>
+    <li><a href="student-profile?page=myprofile">My Profile</a></li>
+    <li><a href="student-profile?page=results">Results</a></li>
+    <li><a href="student-profile?page=progress">Progress</a></li>
+    <li><a href="student-profile?page=mail">Mail</a></li>
+    <li><a href="student-profile?page=edit">Edit</a></li>
   </ul>
 </div>
 
-<div class="container student-info">
-<div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
-  <div class="row">
-    <div class="col-12">
-      <h2>My Profile</h2>
-      <div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
-    </div>
-  </div>
-  <div class="row row-info">
-    <div class="col-sm-6 offset-sm-0 col-md-2 offset-md-1">
-      <ul class="student-header">
-          <li>Student no.:</li>
-          <li>Name:</li>
-          <li>E-mail:</li>
-          <li>Phone number:</li>
-          <li>Address:</li>
-          <li>Role:</li>
-          <li>Teacher:</li>
-          <li>Lesson Package:</li>
-        </ul>
-      </div>
-      <div class="col-sm-6 col-md-3">
-        <ul class="student-info">
-        <li>327068</li>
-        <li>Steven Li</li>
-        <li>327068@student.mboutrecht.nl</li>
-        <li>06123568</li>
-        <li>Teststraat 73</li>
-        <li>Student</li>
-        <li>HSOK</li>
-        <li>Chef</li>
-      </ul>
-      </div>
-    <div class="col-6 vl mt-3 mb-3 mobielhide">
-      <img src="./img/student-picture/default.png" class="img-fluid" alt="Student Image">
-    </div>
-  </div>
-</div>
+<?php 
+if(isset($_GET["page"])) {
+  $page = $_GET["page"];
+  switch($page) {
+    case "myprofile":
+      include("./layout-content/student-profile/myprofile.php");
+      break;
+    case "results":
+      include("./layout-content/student-profile/results.php");
+      break;
+    case "progress":
+      include("./layout-content/student-profile/progress.php");
+      break;
+    case "mail":
+      include("./layout-content/student-profile/mail.php");
+      break;
+    case "edit":
+      include("./layout-content/student-profile/edit.php");
+      break;
+    default:
+      include("./layout-content/student-profile/myprofile.php");
+      break;
+  }
+} else {
+  //Default include without $_GET
+  include("./layout-content/student-profile/myprofile.php");
+}
 
-<div class="container student-options">
-<div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
-  <div class="row">
-    <div class="col-6 mt-3 mb-3">
-      <h1 class="student-text text-upper">Book a work night</h1>
-      <a class="btn btn-outline-george btn-lg" href="#bookshift">Book a shift</a>
-      <a class="btn btn-outline-george btn-lg" href="#signoff">Sign out of shift</a>
-    </div>
-    <div class="col-6 vl mt-3 mb-3">
-      <h1 class="student-text text-upper">Quick Links</h1>
-      <a class="btn btn-outline-george btn-lg" href="https://www.mboutrecht.nl">MBO Utrecht</a>
-      <a class="btn btn-outline-george btn-lg" href="https://www.georgemarina.nl">George Marina</a>
-    </div>
-  </div>
-</div>
-
-<?php include("./includes/footer.php"); ?>
+include("./includes/footer.php"); 
+?>
