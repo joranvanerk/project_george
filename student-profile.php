a678ebe01b4848e48584e3608c3e9b320dc7ccd1
Steven Li <327068@student.mboutrecht.nl>
2021-11-02 11:11:44 +0100

    Added container for student information

--- student-profile.php
+++ student-profile.php
@@ -18,4 +18,36 @@ include("./includes/header.php"); ?>
   </ul>
 </div>
 
+<div class="container student-info">
+  <div class="row">
+    <div class="col-2">
+      <ul>
+        <li>Studentnr:</li>
+        <li>Name:</li>
+        <li>E-mail:</li>
+        <li>Phone number:</li>
+        <li>Address:</li>
+        <li>Role:</li>
+        <li>Teacher:</li>
+        <li>Lesson Package:</li>
+      </ul>
+    </div>
+    <div class="col-8">
+      <ul>
+        <li>327068</li>
+        <li>Steven Li</li>
+        <li>327068@student.mboutrecht.nl</li>
+        <li>06123568</li>
+        <li>Teststraat 73</li>
+        <li>Student</li>
+        <li>HSOK</li>
+        <li>Chef</li>
+      </ul>
+    </div>
+    <div class="col-2">
+      Profile picture
+    </div>
+  </div>
+</div>
+
 <?php include("./includes/footer.php"); ?>
