1da84df45d2d07ce88336e5bea574b80ddd0322a
yusuf <321611@student.mboutrecht.nl>
2021-11-02 14:45:14 +0100

    logout gemaakt

--- logout.php
+++ logout.php
@@ -1,1 +1,16 @@
+<?php
+session_destroy();
+
+include_once("./includes/framework.php");
+include("./classes/functions.php");
+include("./classes/connectDB.php");
+include_once("./includes/header.php");
+
+?>
+
+
 <h1 class="george_title">You have logged out!</h1>
+
+
+<!-- include footer -->
+<?php include_once("./includes/footer.php"); ?>
