0599a49120bd570aa506103200e1807000a479a5
Steven Li <327068@student.mboutrecht.nl>
2021-11-02 11:02:18 +0100

    Added navbar for student profile

--- student-profile.php
+++ student-profile.php
@@ -1,1 +1,21 @@
-<h1>Student profile</h1>
+<?php 
+// Include framework
+include("./includes/framework.php");
+
+// basefile management system for header
+$active_page_filename = basename(__FILE__);
+
+// Include header
+include("./includes/header.php"); ?>
+
+<div class="container">
+  <ul class="student-nav">
+    <li><a href="student-profile">My Profile</a></li>
+    <li><a href="student-profile#results">Results</a></li>
+    <li><a href="student-profile#progress">Progress</a></li>
+    <li><a href="student-profile#mail">Mail</a></li>
+    <li><a href="student-profile#edit">Edit</a></li>
+  </ul>
+</div>
+
+<?php include("./includes/footer.php"); ?>
