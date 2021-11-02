b219897ad7f6c96ca1d1315822f15539c9b74a7d
Steven Li <327068@student.mboutrecht.nl>
2021-11-02 16:14:21 +0100

    Added profile picture

--- student-profile.php
+++ student-profile.php
@@ -19,21 +19,28 @@ include("./includes/header.php"); ?>
 </div>
 
 <div class="container student-info">
+<div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
   <div class="row">
-    <div class="col-2">
-      <ul>
-        <li>Studentnr:</li>
-        <li>Name:</li>
-        <li>E-mail:</li>
-        <li>Phone number:</li>
-        <li>Address:</li>
-        <li>Role:</li>
-        <li>Teacher:</li>
-        <li>Lesson Package:</li>
-      </ul>
+    <div class="col-12">
+      <h2>My Profile</h2>
+      <div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
     </div>
-    <div class="col-8">
-      <ul>
+  </div>
+  <div class="row row-info">
+    <div class="col-sm-6 offset-sm-0 col-md-2 offset-md-1">
+      <ul class="student-header">
+          <li>Student no.:</li>
+          <li>Name:</li>
+          <li>E-mail:</li>
+          <li>Phone number:</li>
+          <li>Address:</li>
+          <li>Role:</li>
+          <li>Teacher:</li>
+          <li>Lesson Package:</li>
+        </ul>
+      </div>
+      <div class="col-sm-6 col-md-3">
+        <ul class="student-info">
         <li>327068</li>
         <li>Steven Li</li>
         <li>327068@student.mboutrecht.nl</li>
@@ -43,9 +50,25 @@ include("./includes/header.php"); ?>
         <li>HSOK</li>
         <li>Chef</li>
       </ul>
+      </div>
+    <div class="col-6 vl mt-3 mb-3 mobielhide">
+      <img src="./img/student-picture/default.png" class="img-fluid" alt="Student Image">
     </div>
-    <div class="col-2">
-      Profile picture
+  </div>
+</div>
+
+<div class="container student-options">
+<div class="mb-2" style="background-color:#000000; height:1px; width:100%;"></div>
+  <div class="row">
+    <div class="col-6 mt-3 mb-3">
+      <h1 class="student-text text-upper">Book a work night</h1>
+      <a class="btn btn-outline-george btn-lg" href="#bookshift">Book a shift</a>
+      <a class="btn btn-outline-george btn-lg" href="#signoff">Sign out of shift</a>
+    </div>
+    <div class="col-6 vl mt-3 mb-3">
+      <h1 class="student-text text-upper">Quick Links</h1>
+      <a class="btn btn-outline-george btn-lg" href="https://www.mboutrecht.nl">MBO Utrecht</a>
+      <a class="btn btn-outline-george btn-lg" href="https://www.georgemarina.nl">George Marina</a>
     </div>
   </div>
 </div>
