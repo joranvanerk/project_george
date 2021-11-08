dae82c7f97caa4f359dc9661f60be8cb1431f7d4
Steven Li <327068@student.mboutrecht.nl>
2021-11-08 11:44:55 +0100

    Added if else for different modal data

--- layout-content/student-profile/edit-script.php
+++ layout-content/student-profile/edit-script.php
@@ -1,2 +1,12 @@
-<h1>Edit script for student
-</h1>
+<?php
+//Security has to be added so only student has access to these pages
+if (isset($_POST["personaldetails"])) {
+echo "personald";
+} else if (isset($_POST["changepassword"])) {
+echo "pw";
+} else if (isset($_POST["changepackage"])) {
+echo "packge";
+} else {
+  echo "Error 404";
+}
+?>
