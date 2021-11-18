d155115ecd7fe1ebacb0826758ad6ab4aa093793
Steven Li <327068@student.mboutrecht.nl>
2021-11-18 09:43:04 +0100

    Changed select query for student-profile and edit

--- classes/logged_in.php
+++ classes/logged_in.php
@@ -1,8 +1,37 @@
 <?php
   class logged_in 
   {
+    public $userrole;
+
     public function __construct() 
     {
+      $this->check_session();
+    }
+
+    //Checks for a session and determites if user is logged in.
+    public function check_session() 
+    {
+      //Case if $_SESSION logged in variable is active
+      if (isset($_SESSION)) 
+      {
+        if ($_SESSION["logged_in"] === true)
+        {
+          $userrole = $_SESSION["userrole"];
+        }
+      } 
+      //Case if $_COOKIE logged in variable is active
+      else if (isset($_COOKIE)) 
+      {
+        if ($_COOKIE["logged_in"] === true)
+        {
+          $userrole = $_COOKIE["userrole"];
+        }
+      } 
+      else 
+      {
+        return "User is not logged in using session nor cookie.";
+      }
+
       if (isset($_SESSION) || isset($_COOKIE)) 
       {
         if ($_SESSION["logged_in"] == true || $_COOKIE["logged_in"] == true) 
@@ -12,9 +41,9 @@
       }
     }
 
-    public function logged_in() 
+    public function has_access($userrole) 
     {
-
+      
     }
 
 
