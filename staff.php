dc2ee0f293a6ed4026c1e0cbf91244b3b633edce
Joranvanerk <joranvanerk1@gmail.com>
2021-11-29 14:32:20 +0100

    Update staff.php
    added basic file content

---
+++ staff.php
@@ -0,0 +1,9 @@
+
+<!-- include framework css and bootstrap basic -->
+<?php include_once("./includes/framework.php");
+$active_page_filename = basename(__FILE__);?>
+<?php include_once("./includes/header.php"); ?>
+
+
+<!-- include footer -->
+<?php include_once("./includes/footer.php"); ?>
