30cf2a24fb4d48ed052a02ab6a41723640ff21db
Steven Li <327068@student.mboutrecht.nl>
2021-12-02 09:38:49 +0100

    Created mail overview HTML based on database information

--- classes/studentMail.php
+++ classes/studentMail.php
@@ -1,17 +1,27 @@
 <?php
-  require_once './classes/userController.php';
+  require_once './classes/connectDB.php';
 
-  class Overview {
+  class MailOverview {
     protected $html;
 
+    //Create overview and display it
     public function __construct() {
+      $this->getRows();
       $this->createOverview();
       $this->show();
     }
 
+    //Retrieve all rows 
+    protected function getRows() {
+
+    }
+
+    //Create overview in $html
     protected function createOverview() {
-      $this->html = '<div class="student-mail">';
+      $this->html = '<div class="row mail-rows">';
+      $this->html .= '<div class="student-mail">';
       $this->html .= '<a href="student-mail?content=hashed_number">Subject: Aanpassing lesrooster <strong>Datum: 17-12-2021</strong><strong>Sender: Hans Odijk</strong></a>';
+      $this->html .= '</div>';
       $this->html .= '</div>';
 
       return $this->html;
@@ -23,4 +33,55 @@
     }
   }
 
+  //Create mailsearch select
+  class MailSearch extends userData{
+    protected $html;
+    protected $data = [];
+
+    //Initialize functions
+    public function __construct() {
+      $this->getUsers();
+      $this->createHTML();
+      $this->show();
+    }
+
+    //Retrieve all mboutrecht mail senders in an array
+    protected function getUsers() {
+      global $conn;
+      $sql = "SELECT * FROM `medewerker`";
+      $query = mysqli_query($conn, $sql);
+      $this->data = mysqli_fetch_all($query, MYSQLI_ASSOC);
+      var_dump($this->data);
+
+      return $this->data;
+    }
+
+    //Create HTML select with all mail senders
+    protected function createHTML() {
+      $this->html =  "<div class='row'>";
+      $this->html .= "<div class='mb-3' style='background-color:#000000; height:1px; width:99%;'></div>";
+      $this->html .= "<h1>Mail</h1>";
+      $this->html .= "<div class='mb-3' style='background-color:#000000; height:1px; width:99%;'></div>";
+      $this->html .= "<form action='' method='get'>";
+      $this->html .= "<input type='hidden' name='page' value='mail'>";
+      $this->html .= "<div class='form-floating mb-3'>";
+      $this->html .= "<select name='search' class='form-control' id='floatingInput'>";
+      foreach ($this->data as $d) {
+        $this->html .= "<option value='".$d["afkorting"]."'>".$d["voornaam"]." ".$d["achternaam"]."</option>";
+      }
+      $this->html .= "</select>";
+      $this->html .= "<label for='floatingInput'>Search messages from:</label>";
+      $this->html .= "</div>";
+      $this->html .= "<div class='form-floating mb-3'>";
+      $this->html .= "<button type='submit' class='btn btn-outline-george'>Search</button>";
+      $this->html .= "<a class='btn btn-cancel' href='student-profile?page=mail'>Remove searchdata</a>";
+      $this->html .= "</div>";
+      $this->html .= "</form>";
+      $this->html .= "</div>";
+    }
+
+    protected function show() {
+      echo $this->html;
+    }
+  }
 ?>
--- layout-content/student-profile/mail.php
+++ layout-content/student-profile/mail.php
@@ -1,38 +1,11 @@
 <?php
-
+require_once './classes/studentMail.php';
 ?>
 <!-- Black line -->
 <!-- <div class="mb-3" style="background-color:#000000; height:1px; width:100%;"></div> -->
 <div class="container mb-1 george_modal">
-  <div class="row">
-    <div class="mb-3" style="background-color:#000000; height:1px; width:99%;"></div>
-    <h1>Mail</h1>
-    <div class="mb-3" style="background-color:#000000; height:1px; width:99%;"></div>
-    <form action="" method="get">
-      <input type="hidden" name="page" value="mail">
-      <div class="form-floating mb-3">
-        <select name="search" class="form-control" id="floatingInput">
-          <option value='HSOK'>Hans</option>
-          <option value='JEMX'>Jesse</option>
-          <option value='TAIF'>Taif</option>
-        </select> 
-        <label for="floatingInput">Search messages from:</label>
-      </div>
-      <div class="form-floating mb-3">
-        <button type="submit" class="btn btn-outline-george">Search</button>
-        <a class="btn btn-cancel" href="student-profile?page=mail">Remove searchdata</a>
-      </div>
-    </form>
-  </div>
-  <div class="row mail-rows">
-    <div class="student-mail">
-      <a href="student-mail?content=hashed_number">Subject: Aanpassing lesrooster <strong>Datum: 17-12-2021</strong><strong>Sender: Hans Odijk</strong></a>
-    </div>
-    <div class="student-mail">
-      <a href="student-mail?content=hashed_number">Subject: Aanpassing lesrooster <strong>Datum: 17-12-2021</strong><strong>Sender: Hans Odijk</strong></a>
-    </div>
-    <div class="student-mail">
-      <a href="student-mail?content=hashed_number">Subject: Aanpassing lesrooster <strong>Datum: 17-12-2021</strong><strong>Sender: Hans Odijk</strong></a>
-    </div>
-  </div>
+  <!-- Generate searchmail row -->
+  <?php new MailSearch; ?>
+  <!-- Generate mailoverview row(s)-->
+  <?php new MailOverview; ?>
 </div>
