1e34f236e5590ff116be323eb0ba36c2cc04298d
Turan <330506@student.mboutrecht.nl>
2021-12-06 09:16:42 +0100

    Added new system for calendar

---
+++ calendar/delete.php
@@ -0,0 +1,18 @@
+<?php
+
+//delete.php
+if(isset($_POST["id"]))
+{
+ $connect = new PDO('mysql:host=localhost;dbname=test_GeorgeMboUtrecht', 'GeorgeUser', 'George123');
+ $query = "
+ DELETE from events WHERE id=:id
+ ";
+ $statement = $connect->prepare($query);
+ $statement->execute(
+  array(
+   ':id' => $_POST['id']
+  )
+ );
+}
+
+?>
