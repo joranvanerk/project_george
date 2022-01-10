1e05232249adf3a52b9123ae9947fd7fcff556b5
yusuf <321611@student.mboutrecht.nl>
2022-01-10 18:22:13 +0100

    review page aangemaakt

---
+++ reviewpage.php
@@ -0,0 +1,65 @@
+<!-- include framework css and bootstrap basic -->
+<?php include_once("./includes/framework.php");
+
+// basefile management system for header
+$active_page_filename = basename(__FILE__);
+?>
+
+<!-- include header -->
+<?php include_once("./includes/header.php");?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Font Awesome Icon Library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.checked {
+  color: orange;
+}
+
+.fa-star{
+  
+}
+</style>
+</head>
+<body>
+
+<h2>Beoordeel ons!</h2>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- include footer -->
+<?php include_once("./includes/footer.php"); ?> 
